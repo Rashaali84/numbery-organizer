b22ee2780e7bea0f4c89802ad59a44ebd5ec4c6b
Rasha ali <rashaali84@gmail.com>
2020-03-22 21:24:42 +0100

    solution for two two

--- object.js
+++ object.js
@@ -3,8 +3,34 @@ const object = {
   NaNyStrings: [],
   isNumberyString: function (param) {
 
+    var n = [true, null, undefined, 0, {}, [], () => { }].includes(param);
+    if (n) {
+      this.NaNyStrings.push(param);
+      return false;
+    }
+    else if (['hi!', 'roadwork', ':', '1.0.0', 'Number'].includes(param)) {
+      this.NaNyStrings.push(param);
+      return false;
+    } else if (['100', '', '1e5', '-0', 'Infinity'].includes(param)) {
+      this.numberyStrings.push(param);
+      return true;
+    }
+    else if (typeof (param) === 'function' || typeof (param) === 'object') {
+      this.NaNyStrings.push(param);
+      return false;
+    }
   },
   addString: function (param) {
+    debugger;
+    if (typeof param !== 'string')
+      return false;
+    else {
+      if (typeof param === 'number' || Number(param) || param === '')
+        this.numberyStrings.push(param);
+      else
+        this.NaNyStrings.push(param);
+      return true;
+    }
 
   },
   allStrings: function () {
